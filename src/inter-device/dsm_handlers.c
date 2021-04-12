80182c9cd95228a0631c811aa2194fb7896859cf
Dong Du <ddnirvana1@gmail.com>
2021-04-12 09:50:10 +0800

    dsm_handlers
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ src/inter-device/dsm_handlers.c
@@ -0,0 +1,23 @@
+/*
+ * This is the handlers of DSM routines in Molecule
+ *
+ * 	Authors: Dong Du
+ * */
+#include<stdio.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<sys/epoll.h>
+#include<unistd.h>
+#include<ctype.h>
+#include<string.h>
+#include<sys/shm.h>
+
+#include <chos/errno.h>
+
+/* DSM handlers list */
+//int syscall_fifo_init(int local_uuid, int owner_pid);
+//int syscall_fifo_close(int global_fifo);
+//int syscall_fifo_read(int global_fifo, int shmid, int length);
+//int syscall_fifo_write(int global_fifo, int shmid, int length);
+
+//End of DSM handler list
