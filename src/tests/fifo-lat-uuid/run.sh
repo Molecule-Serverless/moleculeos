0a125c779eaa4e0460a8c38ff817dd23ada1e46e
Dong Du <ddnirvana1@gmail.com>
2021-04-20 11:30:22 +0800

    tests: add unit test for gfifo_uuid
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ src/tests/fifo-lat-uuid/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+./moleculeos-dist -i 0 &
+sleep 2
+
+./moleculeos-dist -i 1 &
+sleep 2
+
+./test-fifo-lat-server &
+sleep 2
+
+# Here, we assum the global-fifo used by server is #1
+./test-fifo-lat-client -i 1 -p 65260 -s 16 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 32 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 64 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 128 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 256 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 512 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 1024 -c 300
+./test-fifo-lat-client -i 1 -p 65260 -s 2048 -c 300
